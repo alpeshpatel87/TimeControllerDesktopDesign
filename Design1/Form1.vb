e7684c1905b296c3035dd8e3d2393bd5167856ef
Harsh <harshpatel6465@gmail.com>
2022-01-11 15:57:01 +0530

    Done

--- Design1/Form1.vb
+++ Design1/Form1.vb
@@ -18,4 +18,8 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
